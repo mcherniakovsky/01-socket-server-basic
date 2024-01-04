78042c08a67cf92e427a48ea788d01e39b4589d7
mcherniakovsky <mcherniakovsky@gmail.com>
2024-01-04 13:05:55 -0300

    commit

--- models/sockets.js
+++ models/sockets.js
@@ -1,22 +1,38 @@
+
+const BandList = require('./band-list')
+
 class Sockets{
     constructor(io){
         this.io = io;
+        this.bandList = new BandList()
         this.socketEvents();
     }
 
     socketEvents(){
         //On Connection
-
         this.io.on('connection', (socket) => {
-            socket.emit('mensaje-bienvenida', {
-                msg: 'bienvenido al server',
-                fecha: new Date()
-            }) 
-            //escuchar evento
-            socket.on('mensaje-to-server', (data)=> {
-                console.log(data)
-                this.io.emit('mensaje-from-server', data)
+            console.log('Cliente Conectado');
+            
+            //Emitir Las Bandas Actuales
+            socket.emit('current-bands', this.bandList.getBands())
+            //votar por la banda
+            socket.on('votar-banda', (id) => {
+                this.bandList.increaseVotes(id)
+                this.io.emit('current-bands', this.bandList.getBands())
             })
+            socket.on('borrar-banda', (id) => {
+                this.bandList.removeBands(id)
+                this.io.emit('current-bands', this.bandList.getBands())
+            })
+            socket.on('cambiar-nombre', ({id, nombre}) => {
+                this.bandList.changeName(id, nombre)
+                this.io.emit('current-bands', this.bandList.getBands())
+            })
+            socket.on('agregar-banda', ({nombre}) => {
+                this.bandList.addBand(nombre)
+                this.io.emit('current-bands', this.bandList.getBands())
+            })
+
         });
     }
     }
